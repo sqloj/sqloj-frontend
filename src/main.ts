8c62f201cfce91da62555733369f0bc59fe32cf9
rogeryoungh <rogeryoungh@foxmail.com>
2022-04-10 10:45:59 +0800

    add lint

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import 'vfonts/Lato.css'
-import 'vfonts/FiraCode.css'
-import naive from 'naive-ui'
-import './mock'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import 'vfonts/Lato.css';
+import 'vfonts/FiraCode.css';
+import naive from 'naive-ui';
+import './mock';
 
 // createApp(App).use(router).mount('#app')
 createApp(App).use(router).use(naive).mount("#app");
