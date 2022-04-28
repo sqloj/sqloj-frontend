12580519171d6b37e2142ea2e08de78cc6d7ad5d
clumsy-sy <1725798537@qq.com>
2022-04-28 18:57:03 +0800

    update mock user

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -5,29 +5,48 @@ import Mock from 'mockjs';
 // Mock函数
 const { mock } = Mock;
 
+const user = [
+  {
+    userid : 'admin',
+    username : '老师',
+    password : '123456',
+    calss : '',
+    admin : 1
+  },
+  {
+    userid : 'stu',
+    username : '学生',
+    password : '123456',
+    class : '',
+    admin : 0
+  }, 
+  {
+    userid : 'tec',
+    username : 't老师',
+    password : '123456',
+    calss : '',
+    admin : 1
+  }
+]
+
+
 // 设置延时
 Mock.setup({
   timeout: 200
 });
 
 // 使用拦截规则拦截命中的请求，mock(url, post/get, 返回的数据);
-// mock(/\/api\/login/, 'post', data)
+
 mock(/\/api\/user\/login/, 'post', (option: any) => {
   const { userid, password } = JSON.parse(option.body);
-  if (userid === 'admin' && password === '123456') {
-    return {
-      message: '请求成功',
-      success: true,
-      username: '老师',
-      admin: 1
-    };
-  } else if (userid === 'stu' && password === '123456') {
-    return {
-      message: '请求成功',
-      success: true,
-      username: '学生',
-      admin: 0
-    };
+  for(let i = 0; i < user.length; i ++) {
+    if(user[i].userid === userid && user[i].password === password) {
+      return {
+        ...user[i],
+        message: '请求成功',
+        success: true
+      }
+    }
   }
   return {
     message: '请求失败',
@@ -37,20 +56,28 @@ mock(/\/api\/user\/login/, 'post', (option: any) => {
 
 mock(/\/api\/student\/insert/, 'post', (option: any) => {
   const { userid, password } = JSON.parse(option.body);
-  console.log(userid, password);
-  if (userid === 'admin' && password === '123456') {
-    return {
-      message: '请求成功',
-      success: true
-    };
-  } else if (userid === 'stu' && password === '123456') {
-    return {
-      message: '请求成功',
-      success: true
-    };
+  for(let i = 0; i < user.length; i ++) {
+    if(user[i].userid === userid) {
+      return {
+        ...user[i],
+        message: '用户ID已存在',
+        success: false
+      }
+    }
   }
   return {
-    message: '请求失败',
-    success: false
+    message: '注册成功',
+    success: true
   };
 });
+
+mock(/\api\/studentlist/, 'post', {
+  'user|5-20': [ 
+    {
+      'id|201800000000-202000000000': 100,
+      name: '@cname',
+      classes: '寄科221',
+      'acnum|1-10': 10
+    }
+  ]
+})
