253d42ad2051b79370b5dd122b1da778b444bdca
rogeryoungh <rogeryoungh@foxmail.com>
2022-06-01 14:19:04 +0800

    update: add cdn

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+import externalGlobals from 'rollup-plugin-external-globals';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -16,5 +18,17 @@ export default defineConfig({
       }
     }
   },
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: ['highlight.js', 'markdown-it', 'monaco-editor'],
+      plugins: [
+        externalGlobals({
+          'highlight.js': 'hljs',
+          'markdown-it': 'markdownit',
+          'monaco-editor': 'monaco'
+        })
+      ]
+    }
+  }
 });
